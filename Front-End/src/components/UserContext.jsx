22a430b62b634d8b7ac4401bf7f574ae012ed5d0
Olbin-sistemas <cocban54@gmail.com>
2023-12-06 17:05:01 -0600

    correcciones en inicio se sesion estudiantes

--- Front-End/src/components/UserContext.jsx
+++ Front-End/src/components/UserContext.jsx
@@ -1,5 +1,4 @@
-// UserContext.js
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useState, useEffect } from 'react';
 
 const UserContext = createContext();
 
@@ -11,16 +10,30 @@ export function UserContextProvider({ children }) {
   const [user, setUser] = useState(null);
   const [numeroCuenta, setNumeroCuenta] = useState(null);
 
+
   const setUserContext = (userData) => {
     setUser(userData);
+    localStorage.setItem('userData', JSON.stringify(userData));
   };
 
   const setNumeroCuentaContext = (number) => {
     setNumeroCuenta(number);
   };
 
+  const clearUserContext = () => {
+    localStorage.removeItem('userData');
+  };
+
+  useEffect(() => {
+    const storedUserData = localStorage.getItem('userData');
+    if (storedUserData) {
+      const userData = JSON.parse(storedUserData);
+      setUser(userData);
+    }
+  }, []);
+
   return (
-    <UserContext.Provider value={{ user, setUserContext, numeroCuenta, setNumeroCuentaContext }}>
+    <UserContext.Provider value={{ user, setUserContext, numeroCuenta, setNumeroCuentaContext, clearUserContext }}>
       {children}
     </UserContext.Provider>
   );
