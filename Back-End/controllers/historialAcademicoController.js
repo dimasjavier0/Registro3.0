7bcc526ae858ca050f0b9b13cf9310a35f422be7
Pavel Wilfredo Salazar Cortez <144082314+PavelSalazar@users.noreply.github.com>
2023-12-09 19:06:39 -0600

    Historial Academico PDF
    PDF historial

--- Back-End/controllers/historialAcademicoController.js
+++ Back-End/controllers/historialAcademicoController.js
@@ -1,103 +1,167 @@
 const db = require('../conections/database');
 const { jsPDF } = require("jspdf");
+const sql = require("mssql");
+const fs = require('fs');
 
+const imageToBase64 = (path) => {
+  return fs.readFileSync(path, 'base64');
+};
+
+//Aca poner la ruta de la foto png
+const imagePath = 'C:\\Users\\Owner\\Desktop\\Escudo_de_la_UNAH.svg.png';
 
 const obtenerDatosEstudiante = async (numCuenta) => {
-    try {
-        await sql.connect(config);
-        const resultadoEstudiante = await sql.query`SELECT num_cuenta, primer_nombre + ' ' + segundo_nombre as nombre, primer_apellido + ' ' + segundo_apellido as apellido, c.nombre_carrera, cr.nombre_centro, indice_academico from estudiantes e inner join personas p on e.id_persona = p.numero_identidad inner join carreras c on e.id_carrera = c.id_carrera inner join centros_regionales cr on e.id_centro_regional = cr.id_centro WHERE num_cuenta = ${numCuenta}`;
-        const resultadoAsignaturas = await sql.query`SELECT a.codigo_asignatura as codigo, a.nombre_asig as nombre, a.unidades_valorativas as uv, me.periodo_academico as periodo, me.nota, CASE WHEN me.nota >= 70 THEN 'APR' ELSE 'RPB' END as observacion from matricula_estudiantes me inner join secciones s on me.id_seccion = s.id_seccion inner join asignaturas a on s.id_asignatura = a.id_asignatura WHERE me.id_estudiante = ${numCuenta}`;
+  try {
+    await db.connect();
+    console.log("Número de cuenta recibido:", numCuenta);
 
-      console.log(resultadoEstudiante);
-      console.log(resultadoAsignaturas);
+    const resultadoEstudiante = await db.query(`SELECT num_cuenta, 
+      (primer_nombre + ' ' + segundo_nombre) as nombre, 
+      (primer_apellido + ' ' + segundo_apellido) as apellido, 
+      c.nombre_carrera, cr.nombre_centro, indice_academico 
+      FROM estudiantes e 
+      INNER JOIN personas p ON e.id_persona = p.numero_identidad 
+      INNER JOIN carreras c ON e.id_carrera = c.id_carrera 
+      INNER JOIN centros_regionales cr ON e.id_centro_regional = cr.id_centro 
+      WHERE num_cuenta = '${numCuenta}'`);
 
-        return {
-          estudiante: resultadoEstudiante.recordset[0],
-          asignaturas: resultadoAsignaturas.recordset
-        };
-      } catch (err) {
-        console.error(err);
-      } finally {
-        await sql.close();
-      }
-};
+    console.log("Resultado de la consulta de estudiante:", resultadoEstudiante);
 
-//const generarHistorialAcademicoPDF = (estudiante, asignaturas) => {
-  // Lógica para generar el PDF
-//};
+    const resultadoAsignaturas = await db.query(`select a.nombre_asig, a.unidades_valorativas as uv, me.nota, aPAC.id_periodo as periodo
+      from matricula_estudiantes me
+      inner join secciones s on s.id_seccion = me.id_seccion
+      inner join asignaturas a on a.id_asignatura = s.id_asignatura
+      inner join Asignaturas_PAC aPAC on aPAC.id_asignatura_pac = s.id_asignatura
+      where me.id_estudiante = '${numCuenta}'`);
 
-exports.generarPDF = async (req, res) => {
-  const numCuenta = req.params.numCuenta;
+    console.log("Resultado de la consulta de asignaturas:", resultadoAsignaturas);
 
-  const obtenerDatosEstudiante = async (numCuenta) => {
-    try {
-        await sql.connect(config);
-        const resultadoEstudiante = await sql.query`SELECT num_cuenta, primer_nombre + ' ' + segundo_nombre as nombre, primer_apellido + ' ' + segundo_apellido as apellido, c.nombre_carrera, cr.nombre_centro, indice_academico from estudiantes e inner join personas p on e.id_persona = p.numero_identidad inner join carreras c on e.id_carrera = c.id_carrera inner join centros_regionales cr on e.id_centro_regional = cr.id_centro WHERE num_cuenta = ${numCuenta}`;
-        const resultadoAsignaturas = await sql.query`SELECT a.codigo_asignatura as codigo, a.nombre_asig as nombre, a.unidades_valorativas as uv, me.periodo_academico as periodo, me.nota, CASE WHEN me.nota >= 70 THEN 'APR' ELSE 'RPB' END as observacion from matricula_estudiantes me inner join secciones s on me.id_seccion = s.id_seccion inner join asignaturas a on s.id_asignatura = a.id_asignatura WHERE me.id_estudiante = ${numCuenta}`;
-    
-        return {
-          estudiante: resultadoEstudiante.recordset[0],
-          asignaturas: resultadoAsignaturas.recordset
-        };
-    } catch (err) {
-        console.error(err);
-    } finally {
-        await sql.close();
-    }
-};
+    // Retorna un objeto con las propiedades estudiante y asignaturas
+    return {
+      estudiante: resultadoEstudiante,
+      asignaturas: resultadoAsignaturas
+    };
+  } catch (err) {
+    console.error("error al obtener datos del estudiante.. :c", err);
 
-const generarHistorialAcademicoPDF = (estudiante, asignaturas) => {
-    const doc = new jsPDF();
-  
-    // Configura el título y los detalles del estudiante
-    doc.setFontSize(16);
-    doc.text("Historial Académico", 20, 20);
-    doc.setFontSize(12);
-    doc.text(`Cuenta: ${estudiante.numCuenta}`, 20, 30);
-    doc.text(`Nombre: ${estudiante.nombre}`, 20, 40);
-    doc.text(`Apellido: ${estudiante.apellido}`, 20, 50);
-    doc.text(`Carrera Actual: ${estudiante.carrera}`, 20, 60);
-  
-    // Configura los encabezados de la tabla
-    doc.setFontSize(10);
-    doc.text("Código", 20, 70);
-    doc.text("Nombre", 50, 70);
-    doc.text("UV", 130, 70);
-    doc.text("Período", 150, 70);
-    doc.text("Nota", 170, 70);
-  
-    // Agrega las asignaturas
-    let y = 80;
-    asignaturas.forEach(asignatura => {
-      doc.text(asignatura.codigo, 20, y);
-      doc.text(asignatura.nombre, 50, y);
-      doc.text(asignatura.uv.toString(), 130, y);
-      doc.text(asignatura.periodo, 150, y);
-      doc.text(asignatura.nota.toString(), 170, y);
-      y += 10;
-    });
-  
-    const buffer = doc.output("arraybuffer");
-    console.log("PDF generado correctamente");
-    return buffer;
+    // En caso de error, también retorna un objeto con las propiedades estudiante y asignaturas, pero con valores vacíos
+    return {
+      estudiante: [],
+      asignaturas: []
+    };
+  } finally {
+    await db.close();
+  }
 };
 
 exports.generarPDF = async (req, res) => {
+  console.log("Petición recibida para generar PDF");
   const numCuenta = req.params.numCuenta;
+  console.log("Generando PDF para:", numCuenta);
 
   try {
     const datos = await obtenerDatosEstudiante(numCuenta);
-    if (datos && datos.estudiante && datos.asignaturas) {
-      const pdfBuffer = generarHistorialAcademicoPDF(datos.estudiante, datos.asignaturas);
-      console.log("Enviando PDF...");
+    console.log("Datos obtenidos para", numCuenta, ":", datos);
+
+    if (datos && datos.estudiante && datos.estudiante.length > 0 && datos.asignaturas && datos.asignaturas.length > 0) {
+      console.log("Generando el PDF...");
+      const estudiante = datos.estudiante[0];
+      const asignaturas = datos.asignaturas;
+
+      const pdfBuffer = generarHistorialAcademicoPDF(estudiante, asignaturas);
+
+      console.log("PDF generado, enviando respuesta...");
       res.setHeader('Content-Type', 'application/pdf');
       res.send(Buffer.from(pdfBuffer));
-  } else {
+    } else {
+      console.log("No se encontraron datos para el estudiante:", numCuenta);
       res.status(404).send('No se encontraron datos para el estudiante');
     }
   } catch (error) {
-    console.error(error);
+    console.error("Error al generar el PDF para la cuenta", numCuenta, ":", error);
     res.status(500).send('Error al generar el historial académico');
   }
 };
+
+const generarHistorialAcademicoPDF = (estudiante, asignaturas) => {
+  const doc = new jsPDF();
+  const logo = imageToBase64(imagePath);
+
+  // Configurar el título del documento y los encabezados
+  doc.setFontSize(12);
+  doc.setFont('helvetica', 'bold');
+  doc.text('UNIVERSIDAD NACIONAL AUTÓNOMA DE HONDURAS', 105, 10, null, null, 'center');
+  doc.setFontSize(10);
+  doc.text('DIRECCIÓN DE INGRESOS PERMANENCIA Y PROMOCIÓN', 105, 16, null, null, 'center');
+  doc.text('HISTORIAL ACADÉMICO', 105, 22, null, null, 'center');
+
+  
+  doc.addImage(logo, 'PNG', 30, 5, 12, 17);
+
+
+  // Información del estudiante con rectángulos grises y esquinas redondeadas
+  doc.setFillColor(224, 224, 224); // Gris claro
+  doc.roundedRect(20, 28, 90, 18, 2, 2, 'F');
+  doc.roundedRect(111, 28, 90, 18, 2, 2, 'F');
+
+  // Asegurar que la información del estudiante esté en mayúsculas y no sea undefined
+  doc.setFont('helvetica', 'normal');
+  doc.setFontSize(10);
+  doc.text(`Cuenta: ${estudiante.num_cuenta ? estudiante.num_cuenta.toUpperCase() : ''}`, 22, 33);
+  doc.text(`Nombre: ${estudiante.nombre ? estudiante.nombre.toUpperCase() : ''}`, 22, 38);
+  doc.text(`Apellido: ${estudiante.apellido ? estudiante.apellido.toUpperCase() : ''}`, 22, 43);
+  doc.text(`Carrera Actual: ${estudiante.nombre_carrera ? estudiante.nombre_carrera.toUpperCase() : ''}`, 122, 33);
+  doc.text(`Centro: ${estudiante.nombre_centro ? estudiante.nombre_centro.toUpperCase() : ''}`, 122, 38);
+  doc.text(`Índice: ${estudiante.indice_academico ? estudiante.indice_academico.toString().toUpperCase() : 'NO DISPONIBLE'}`, 122, 43);
+
+  // Rectángulo gris detrás del nombre de la carrera
+  doc.setFillColor(224, 224, 224);
+  doc.roundedRect(20, 50, 180, 6, 1, 1, 'F');
+  
+  // Nombre de la carrera en mayúsculas y centrado
+  doc.setFont('helvetica', 'bold');
+  doc.setTextColor(0);
+  doc.text(estudiante.nombre_carrera.toUpperCase(), 105, 55, null, null, 'center');
+
+  // Año y línea divisoria
+  doc.setFontSize(10);
+  doc.setFont('helvetica', 'bold');
+  doc.text('2019', 105, 62, null, null, 'center');
+  doc.setLineWidth(0.2);
+  doc.line(20, 64, 190, 64); // Cambia las coordenadas según sea necesario
+
+  // Encabezados de las columnas en mayúsculas
+  doc.setTextColor(0);
+  doc.text('CÓDIGO', 20, 70);
+  doc.text('NOMBRE', 50, 70);
+  doc.text('UV', 120, 70);
+  doc.text('PERÍODO', 140, 70);
+  doc.text('NOTA', 160, 70);
+  doc.text('OBS', 180, 70);
+
+  // Datos de las asignaturas
+  let y = 75;
+  let totalAprobadas = 0;
+  asignaturas.forEach(asignatura => {
+    let obs = asignatura.nota >= 65 ? 'APR' : 'RPB'; // Establece APR o RPB según la nota
+    if (obs === 'APR') totalAprobadas++;
+    doc.text(asignatura.codigo_asignatura || '', 20, y);
+    doc.text(asignatura.nombre_asig || '', 50, y, { maxWidth: 60 });
+    doc.text(asignatura.uv.toString() || '', 120, y);
+    doc.text(asignatura.periodo.toString() || '', 140, y);
+    doc.text(asignatura.nota.toString() || '', 160, y);
+    doc.text(obs, 180, y);
+    y += 5;
+  });
+
+  doc.setFont('helvetica', 'bold');
+  doc.text(`Total Aprobadas: ${totalAprobadas}`, 20, y);
+
+  // Pie de página en mayúsculas y centrado
+  doc.setFontSize(8);
+  doc.text('La Educación es la Primera Necesidad de la República'.toUpperCase(), 105, 285, null, null, 'center');
+    let pageCount = doc.internal.getNumberOfPages(); // Obtiene el número total de páginas
+  doc.text(`Página 1 de ${pageCount}`, 20, 290);
+  // Retorna el PDF como un buffer
+  return doc.output('arraybuffer');
 };
